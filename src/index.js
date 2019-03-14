5831ab32f988c63d523532d6b18c6dcec6403955
DarkH0le <andresivan.aldama@gmail.com>
2019-03-13 18:55:59 -0600

    Pass state as a prop

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-//Compose Components
-
-//What is compose components?
-/*
- * This refers to a component that display one or more child components
- */
+//Intro to state
 
 //Import dependecies
 
-import React from 'react';
+import React , {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 //Set a list of "Books"
@@ -16,7 +11,7 @@ let bookList = [
     {
         "title": "IT",
         "by": "Sthepen",
-        "pages": 2334
+        "pages": 234
     },
     {
         "title": "Harry Potter",
@@ -32,29 +27,53 @@ let bookList = [
 
 
 //Create a component
-const Book = ({title,author,pages}) => {
+const Book = ({title,author,pages,freeBookMark}) => {
     return(
         <div>
             <h2>{title}</h2>
             <p>by: {author}</p>
             <p>Pages: {pages}</p>
+            <p>Free? {freeBookMark ? 'YES!' : 'NO'}</p>
         </div>
     );
 };
 
-//Create a compose component, this "library" component is going to be made by "book"s components
+//Create a ES6 class and give it a state using this.state
 //Note: Keys used within arrays should be unique among their siblings. However they don’t need to be globally unique. We can use the same keys when we produce two different arrays
 
+class Library extends Component {
 
-const Library = ({bookList}) => {
-    return (
-        <div>
-            {bookList.map((book , i) => {
-                            return (<Book key={i} title={book.title} author={book.author} pages={book.pages}/>);
-                         })}
-        </div>
-    );
-};
+    /* constructor(props){
+        super(props)
+        this.state = {
+            "open" : true
+        }
+        }*/
+
+    //Setting up the state, same as above
+    state = {"open":true,"freeBookMark":false}
+
+    toggleOpenClosed = () => {
+        this.setState({
+            "open" : !this.state.open
+        });
+    }
+
+    render(){
+        console.log(this.state);
+        const {bookList} = this.props
+        return (
+            <div>
+                <h1> The library is {this.state.open ? 'open':'closed'}!!!</h1>
+                {bookList.map((book , i) => {
+                                return (<Book key={i} title={book.title} author={book.author} pages={book.pages} freeBookMark={this.state.freeBookMark}/>);
+                })}
+                <button onClick={this.toggleOpenClosed}></button>
+            </div>
+        );
+      
+    }
+}
 
 ReactDOM.render(<Library bookList={bookList}/>,document.querySelector('#root'));
 
